717760165057f24f5debdbfb45e93bd99be5a60f
Yu-Hsin Wu <109631679+yuliana1123@users.noreply.github.com>
2024-03-27 00:47:30 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,20 +3,23 @@
 #include <vector>
 #include "Dungeon.h"
 #include "Dungeon.cpp"
-#include "Item.h"
+#include "Player.h"
+#include "GameCharacter.h"
 #include "Monster.h"
 #include "NPC.h"
-#include "Player.h"
+#include "Endingpos.h"
+#include "Object.h"
+#include "Object.cpp"
+#include "Trading.h"
 
 
 using namespace std;
 
 
 int main(){
-    /*Dungeon dungeon = Dungeon();
-    dungeon.runDungeon();*/
-    welcome();
-    createPlayer();
+
+    Dungeon dungeon = Dungeon();
+    dungeon.runDungeon();
 
     return 0;
 }
